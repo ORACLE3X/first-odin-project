382842dc77276663bcd2b69b407211e93290947a
ORACLE3X <legendaryoracle8@gmail.com>
2025-12-15 15:22:24 +0100

    worked on first and third project

---
+++ second_project/project_resources/assets.js
@@ -0,0 +1,19 @@
+// import dashboard from './dashboard.svg';
+// import support from './support.svg';
+// import settings from './setting.svg';
+// import profile from './profile.svg';
+// import search from './search.svg';
+// import home from './home.svg';
+// import community from './community.svg';
+// import history from './history.svg';
+// import logo from './logo.png';
+// import notification from './notification.svg';
+// import goat from './goat.png';
+// import message from './message.svg';
+// import task from './task.svg';
+// import search from './search.svg';
+
+
+// export const assets = {
+//   dashboard,support,settings,profile,search,home,community,history,logo,notification,goat,message,task,search
+// };
